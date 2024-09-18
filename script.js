37da948cdacc9efc805cc47b97283593334704bb
Eduardo Florenciano dos Santos <127545659+eduardoflorencianosantos@users.noreply.github.com>
2024-09-18 12:10:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function calcula(){
+    const a = document.getElementById('valor_a').value;
+    const b = document.getElementById('valor_b').value;
+    const c = document.getElementById('valor_c').value;
+
+    let delta = (b*b) - (4*a*c)
+}
